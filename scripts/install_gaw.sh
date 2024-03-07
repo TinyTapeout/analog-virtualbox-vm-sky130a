b27c88950bda735895e4f30cad071cc2cf9c8232
Uri Shaked <uri@urishaked.com>
2024-03-08 01:54:29 +0200

    fix(gaw): build issues

--- scripts/install_gaw.sh
+++ scripts/install_gaw.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libgtk-3-dev gettext automake
 
 git clone https://github.com/StefanSchippers/xschem-gaw /tmp/xschem-gaw
 cd /tmp/xschem-gaw
+sed -i 's/GETTEXT_MACRO_VERSION = 0.18/GETTEXT_MACRO_VERSION = 0.20/' po/Makefile.in.in
+aclocal && autoconf && autoheader && automake --add-missing
 ./configure
 make -j4
 sudo make install
