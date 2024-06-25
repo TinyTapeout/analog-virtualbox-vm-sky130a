7f5294632a1d1ab5b07227b8c29234cb83959990
Matt Venn <matt@mattvenn.net>
2024-06-25 17:28:05 +0200

    fix apt install

--- scripts/install_verilator.sh
+++ scripts/install_verilator.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-sudo apt install git make autoconf g++ flex bison help2man
+sudo apt install -y git make autoconf g++ flex bison help2man
 
-git clone https://github.com/verilator/verilator verilator-src
-cd verilator-src
+git clone https://github.com/verilator/verilator /tmp/verilator-src
+cd /tmp/verilator-src
 unset VERILATOR_ROOT
 git checkout $VERILATOR_VERSION
 autoconf
